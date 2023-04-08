e032a5f77d9ef5edffd9a91fd6f7c25f4079868d
Joao-Galdino <joaogaldyno@gmail.com>
2023-04-08 08:58:23 -0300

    feat: Create a films view

--- films/views.py
+++ films/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 from django.contrib.auth import get_user_model
+from django.views.generic.list import ListView
 
 from films.forms import RegisterForm
+from films.models import Film
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -23,9 +25,31 @@ class RegisterView(FormView):
         form.save()  # save the user
         return super().form_valid(form)
 
+
+class FilmList(ListView):
+    template_name = "films.html"
+    model = Film
+    context_object_name = "films"
+
+    def get_queryset(self):
+        return Film.objects.filter(user=self.request.user)
+
+
 def check_username(request):
     username = request.POST.get('username')
     if get_user_model().objects.filter(username=username).exists():
         return HttpResponse('<div id="username-error" class="error">Username already exists</div') 
     else:
         return HttpResponse('<div id="username-error" class="success">Username is available</div')
+    
+def add_film(request):
+    name = request.POST.get('filmname') #Name of atrribute name in form
+
+    film = Film.objects.create(name=name)
+
+    # add film to user`s list
+    request.user.films.add(film)
+
+    #return tamplate with new film
+    films = request.user.films.all()
+    return render(request, 'partials/film-list.html', {'films': films})
