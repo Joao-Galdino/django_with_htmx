fd3a69d1bf71b2e9a9231d5de303a7af3bbd43a2
Joao-Galdino <joaogaldyno@gmail.com>
2023-04-11 07:35:53 -0300

    feat: Create drag and drop functionality to film list

--- films/views.py
+++ films/views.py
@@ -11,7 +11,8 @@ from django.views.decorators.http import require_http_methods
 from django.contrib import messages
 
 from films.forms import RegisterForm
-from films.models import Film
+from films.models import Film, UserFilms
+from films.utils import get_max_order, reorder
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -36,7 +37,7 @@ class FilmList(LoginRequiredMixin, ListView):
     context_object_name = "films"
 
     def get_queryset(self):
-        return Film.objects.filter(user=self.request.user)
+        return UserFilms.objects.filter(user=self.request.user)
 
 def check_username(request):
     username = request.POST.get('username')
@@ -51,26 +52,38 @@ def add_film(request):
 
     film = Film.objects.get_or_create(name=name)[0]
     # add film to user`s list
-    request.user.films.add(film)
-
+    if not UserFilms.objects.filter(film=film, user=request.user).exists():
+        UserFilms.objects.create(
+            film=film,
+            user=request.user,
+            order=get_max_order(request.user)
+        )
+    
+    
     #return tamplate with new film
-    films = request.user.films.all()
+    films = UserFilms.objects.filter(user=request.user)
     messages.success(request, f'Added {name} to list of films')
     return render(request, 'partials/film-list.html', {'films': films})
 
 @login_required
 @require_http_methods(["DELETE"])
 def delete_film(request, pk):
-    request.user.films.remove(pk)
-    films = request.user.films.all()
-    return render(request, 'partials/film-list.html', {'films': films})
+    # remove the film from the user's list
+    UserFilms.objects.get(pk=pk).delete()
 
+    reorder(request.user)
+
+    # return template fragment with all the user's films
+    films = UserFilms.objects.filter(user=request.user)
+    return render(request, 'partials/film-list.html', {'films': films})
 
 @login_required
 def search_film(request):
     search_text = request.POST.get('search')
+
+    userfilms = UserFilms.objects.filter(user=request.user)
     results = Film.objects.filter(name__icontains=search_text).exclude(
-        name__in = request.user.films.all().values_list('name', flat=True)#exclude films that user already has on his list
+        name__in = userfilms.values_list('film__name', flat=True)#exclude films that user already has on his list
     )
     context = {'results': results}
     return render(request, 'partials/search-results.html', context)
@@ -78,3 +91,16 @@ def search_film(request):
 @login_required
 def clear(request):
     return HttpResponse("")
+
+@login_required
+def sort(request):
+    film_pks_order = request.POST.getlist('film_order')
+    films = []
+    for idx, film_pk in enumerate(film_pks_order, start=1):
+        userfilm = UserFilms.objects.get(pk=film_pk)
+        userfilm.order = idx
+        userfilm.save()
+        films.append(userfilm)
+
+    return render(request, 'partials/film-list.html', {'films': films})
+
